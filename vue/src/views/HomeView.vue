e6bdb023297181ef742919792aaefe2ecb8f424a
Macey Panaro <panaromj@gmail.com>
2024-08-03 15:36:15 -0400

    Updating landing page

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+  <div>
+  </div>
+  <div class="container-fluid p-5 text-dark text-center">
+    <h1>Welcome</h1>
+    <p>Brief description of company and website</p>
+  </div>
+  <div class="row justify-content-sm-center">
+    <div class="col-sm-3 text-center border p-5">Picture 1</div>
+    <div class="col-sm-3 text-center border p-5">Picture 2</div>
+    <div class="col-sm-3 text-center border p-5">Picture 3</div>
   </div>
 </template>
 
