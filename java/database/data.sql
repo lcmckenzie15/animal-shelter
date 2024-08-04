1a25a23cb0cf42b2b3a045815f9744f6459a1097
Thomas Troughton <TAT6464@gmail.com>
2024-08-04 11:43:38 -0400

    committing before lunch break

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (email,password_hash,role, phone_number, date_of_birth, first_name, last_name) VALUES ('user@te.net','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', '123-456-7890', '2024-08-04', 'Test', 'User');
+INSERT INTO users (email,password_hash,role, phone_number, date_of_birth, first_name, last_name) VALUES ('admin@te.net','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', '123-456-7890', '2024-08-04', 'Test', 'Admin');
 
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,56 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS registration;
+DROP TABLE IF EXISTS pets;
+DROP TABLE IF EXISTS photos;
 
 CREATE TABLE users (
 	user_id SERIAL,
-	username varchar(50) NOT NULL UNIQUE,
+	email varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
+	phone_number varchar(12) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	first_name varchar(25) NOT NULL,
+	last_name varchar(25) NOT NULL,
+	password_set boolean DEFAULT FALSE,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+CREATE TABLE registration (
+    registration_id SERIAL,
+    email varchar(50) NOT NULL UNIQUE,
+    phone_number varchar(12) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    first_name varchar(25) NOT NULL,
+    last_name varchar(25) NOT NULL,
+    status varchar(10) DEFAULT 'pending',
+    CONSTRAINT PK_registration PRIMARY KEY (registration_id)
+);
+
+CREATE TABLE pets (
+    pet_id SERIAL,
+    species varchar(3) NOT NULL,
+    gender varchar(6) NOT NULL,
+    age integer NOT NULL,
+    name varchar(25) NOT NULL,
+    breed varchar(25) NOT NULL,
+    pet_size varchar(25) NOT NULL,
+    color varchar(25) NOT NULL,
+    description TEXT,
+    is_adopted boolean DEFAULT FALSE,
+    CONSTRAINT PK_pets PRIMARY KEY (pet_id)
+);
+
+CREATE TABLE photos (
+    photo_id SERIAL,
+    pet_id integer NOT NULL,
+    photo_url TEXT NOT NULL,
+    CONSTRAINT PK_photos PRIMARY KEY (photo_id),
+    CONSTRAINT FK_photos_table FOREIGN KEY (pet_id) REFERENCES pets (pet_id)
+
+);
+
 
 COMMIT TRANSACTION;
