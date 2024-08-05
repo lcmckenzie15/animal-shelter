fb7ca9b5734a7301f764fd3cf1748b7d7f5c176b
Macey Panaro <panaromj@gmail.com>
2024-08-05 14:02:15 -0400

    Footer 90% complete. Updated nav bar. Added logo to nav bar. I am awesome :Macey

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,56 +1,139 @@
 <template>
   <div id="capstone-app" >
     <div id="nav">
+      <img src="@/Pictures/logo.png" alt="Home" class="logo" @click="goHome">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'home' }">Adopt</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'volunteer' }">Volunteer</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
+      <router-link class="login-link" v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
+      <router-link class="login-link" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      </div>
     <router-view />
   </div>
+  <div class="container-fluid my-5">
+
+<footer class="text-center text-white" style="background-color: #ECEFF1;">
+<!-- Grid container -->
+<div class="container pt-4">
+  <section class="mb-4">
+      <!-- Facebook -->
+      <a
+        class="btn btn-link btn-floating btn-lg m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-facebook-f fa-2x" style="color: #3b5998;"></i></a>
+
+      <!-- Twitter -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-twitter fa-2x" style="color: #55acee;"></i></a>
+
+      <!-- YouTube -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-youtube fa-2x" style="color: #ed302f;"></i></a>
+
+      <!-- Instagram -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-instagram fa-2x" style="color: #ac2bac;"></i></a>
+
+      <!-- Linkedin -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-linkedin-in fa-2x" style="color: #0082ca;"></i></a>
+      <!-- Reddit -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-reddit-alien fa-2x" style="color: #ff4500;"></i></a>
+    </section>
+</div>
+<!-- Grid container -->
+
+<!-- Copyright -->
+<div class="text-center text-light p-3" style="background-color: #607D8B;">
+  TechElevator © 2024 Copyright
+</div>
+<!-- Copyright -->
+</footer>
+
+</div>
 </template>
 
 <style>
 :root {
-    --primary-color: #607D8B;
-
-    --secondary-color: #CFD8DC;
-
-    --accent-color: #FF5722;
-
-    --background-color: #ECEFF1;
-
-    --text-color: #263238;
+	--primary-color: #607D8B;
+	--secondary-color: #CFD8DC;
+	--accent-color: #FF5722;
+	--background-color: #ECEFF1;
+	--text-color: #263238;
 }
 #nav {
-  background-color: var(--primary-color);
-  color: var(--secondary-color);
+  background-color: var(--background-color);
+  color: var(--text-color);
   font-size: 25px;
-  padding: 20px 300px; 
-  display: flex; 
-  gap: 20px; 
-  align-items: center; 
+  padding: 5px, 5px;
+  display: flex;
+  align-items: center;
 }
 
-.home-logo {
-  width: 100px;
+
+.logo {
+  width: 300px;
   height: auto;
-cursor: pointer;
+  cursor: pointer;
+  margin-right: auto;
+}
+
+
+.nav-links {
+  display: flex;
+  gap: 20px;
+  flex-grow: 1;
 }
 
 
 a {
-  color: var(--background-color);
+  color: var(--text-color);
   text-decoration: none;
 }
 
+
 a:visited {
-  color: var(--background-color);
+  color: var(--text-color);
   text-decoration: none;
 }
+
 
 a:hover {
   color: var(--accent-color);
 }
+
+
+.login-link {
+  color: var(--text-color);
+  margin-left: auto;
+}
+
+.login-link:visited {
+  color: var(--text-color);
+}
+
 
 </style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -5,6 +5,7 @@
     <h1>Welcome</h1>
     <p>Brief description of company and website</p>
   </div>
+  <!-- Placeholder for pet showcase until API is finished -->
   <div class="row justify-content-sm-center">
     <div class="col-sm-3 text-center border p-5">Picture 1</div>
     <div class="col-sm-3 text-center border p-5">Picture 2</div>
