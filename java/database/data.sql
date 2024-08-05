c4bb9dd2feced8d657ef805f8a25c052c7452613
Thomas Troughton <TAT6464@gmail.com>
2024-08-05 09:30:32 -0400

    back to square 1

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,6 +1,9 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (email,password_hash,role, phone_number, date_of_birth, first_name, last_name) VALUES ('user@te.net','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', '123-456-7890', '2024-08-04', 'Test', 'User');
-INSERT INTO users (email,password_hash,role, phone_number, date_of_birth, first_name, last_name) VALUES ('admin@te.net','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', '123-456-7890', '2024-08-04', 'Test', 'Admin');
+INSERT INTO users (username,password_hash,role) VALUES ('user@te.net','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('admin@te.net','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 COMMIT TRANSACTION;
+
+--INSERT INTO users (email,password_hash,role, phone_number, date_of_birth, first_name, last_name) VALUES ('user@te.net','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', '123-456-7890', '2024-08-04', 'Test', 'User');
+--INSERT INTO users (email,password_hash,role, phone_number, date_of_birth, first_name, last_name) VALUES ('admin@te.net','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', '123-456-7890', '2024-08-04', 'Test', 'Admin');
